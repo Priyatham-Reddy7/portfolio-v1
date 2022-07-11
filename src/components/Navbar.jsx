7820da2c8fd303b1c88b3e6a8b64ad816f741e89
Priyatham Reddy <priyathamreddy147@gmail.com>
2022-07-11 21:09:36 +0530

    Add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,33 @@
+import { Link, useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Reorder } from "@mui/icons-material"
+import "../styles/Navbar.css"
+
 const Navbar = () => {
-    return <div>Navbar</div>
+    const [expandNavbar, setExpandNavbar] = useState(false)
+    const location = useLocation()
+
+    //?sets togglebar to false whenever url changes
+    useEffect(() => {
+        setExpandNavbar(false)
+    }, [location])
+
+    return (
+        <>
+            <div className="navbar" id={expandNavbar ? "open" : "close"}>
+                <div className="toggleButton">
+                    <button onClick={() => setExpandNavbar((prev) => !prev)}>
+                        <Reorder />
+                    </button>
+                </div>
+                <div className="links">
+                    <Link to="/">Home</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/experience">Experience</Link>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,6 @@
-import Navbar from "../components/Navbar"
-
 const Home = () => {
     return (
         <>
-            <Navbar />
             <h1>Home</h1>
         </>
     )
