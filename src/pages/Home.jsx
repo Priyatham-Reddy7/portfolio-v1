80b00d37fd0ffbadb3961b2ee22dba1bec6ba3ba
Priyatham Reddy <priyathamreddy147@gmail.com>
2022-07-11 22:47:40 +0530

    Add Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,49 @@
+import Email from "@mui/icons-material/Email"
+import Linkedin from "@mui/icons-material/Linkedin"
+import Github from "@mui/icons-material/Github"
+import "../pageStyles/Home.css"
+
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+            <div className="home">
+                <div className="about">
+                    <h2>Hi, My Name is Priyatham</h2>
+                    <div className="prompt">
+                        <p>
+                            Software Developer with a passion for learning and
+                            creating
+                        </p>
+                        <Linkedin />
+                        <Github />
+                        <Email />
+                    </div>
+                </div>
+                <div className="skills">
+                    <h1>Skills</h1>
+                    <ol className="list">
+                        <li className="item">
+                            <h2>Front-End</h2>
+                            <span>
+                                HTML, CSS, ReactJS, Bootstrap, MaterialUI,
+                                StyledComponents
+                            </span>
+                        </li>
+                    </ol>
+                    <ol className="list">
+                        <li className="item">
+                            <h2>Back-End</h2>
+                            <span>NodeJS, ExpressJS, MongoDB, MySQL</span>
+                        </li>
+                    </ol>
+                    <ol className="list">
+                        <li className="item">
+                            <h2>Languages</h2>
+                            <span>JavaScript, Python, TypeScript</span>
+                        </li>
+                    </ol>
+                </div>
+            </div>
         </>
     )
 }
