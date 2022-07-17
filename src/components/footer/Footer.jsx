2939067e5bddbfa00727ccc20f4f89c52ce221e1
Priyatham Reddy <priyathamreddy147@gmail.com>
2022-07-17 12:53:25 +0530

    Update Footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,10 +9,15 @@ const Footer = () => {
         <>
             <div className="footer">
                 <div className="socialMedia">
-                    <Instagram />
-                    <Twitter />
-                    <Facebook />
-                    <Linkedin />
+                    <a href="https://www.linkedin.com/in/priyatham-reddy147/">
+                        <Linkedin />
+                    </a>
+                    <a href="https://www.instagram.com/p_r_i_m_y_7/">
+                        <Instagram />
+                    </a>
+                    <a href="https://www.facebook.com/priyatham.reddy.5268/">
+                        <Facebook />
+                    </a>
                 </div>
                 <p>&copy; Priyatham.com</p>
             </div>
